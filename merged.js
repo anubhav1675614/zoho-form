42d3015bea0a3c0af1701b3b8b0783c24d073cc2
Thomas Koch <thomaskoch@Thomass-Mac-mini.local>
2020-05-07 13:55:45 -0500

    initial commit

---
+++ merged.js
@@ -0,0 +1,49 @@
+function openModal() {
+  const urlString = window.location.href;
+  const url = new URL(urlString);
+  const email = url.searchParams.get("Email");
+  const ref = url.searchParams.get("Ref");
+
+  let additionalParams = '';
+  let reqUrl = 'https://creator.zohopublic.com/zoho_jon1217/reg-a-offering/form-embed/INDV_Investment_Form/OwXuOUYyz5O9W5Xa2e01aNY8DNn6FbECK11ZYYU7ZeK8awBEBs2nZ4pfkXneFfU60HjmtnFgBkx10MVkQHVprKAYe7VYjjQJkqX9'
+
+  if (email) {
+    additionalParams = '?Email=' + email;
+  }
+  if (email && ref) {
+    additionalParams += `&Ref=${ref}`;
+  }
+  if (email === null && ref) {
+    additionalParams += `?Ref=${ref}`;
+  }
+
+  reqUrl += additionalParams;
+
+  const instance = basicLightbox.create(
+    `<iframe id="formFrame" frameborder='0' allowTransparency='true' scrolling='auto' src=${reqUrl}></iframe>`, {
+      closable: false
+    }
+  );
+
+  instance.show(() => {
+    setTimeout(() => {
+      const close = document.createElement("div");
+      const frame = document.getElementById("formFrame");
+
+      close.className = "close-button";
+
+      close.style["top"] = frame.offsetTop - 19;
+      close.style["left"] = frame.offsetLeft + frame.offsetWidth - 22;
+
+      close.onclick = ev => instance.close();
+
+      const lightbox = document.querySelector(".basicLightbox");
+
+      lightbox.appendChild(close);
+    }, 1000);
+  });
+}
+
+
+
+
